f228912d0459ec6c15391820c787d4ba677ff3ae
neal <neal@clamra.com>
2012-12-13 22:33:02 +0800

    init version

---
+++ content/pages/contact.rst
@@ -0,0 +1,8 @@
+Contact Clamra Team
+######################
+
+:date: 2012-12-12 22:00
+:category: yeah
+:author: Neal Tao
+
+You can reach us by email team@clamra.com .
---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Clamra Team'
+SITENAME = u'Clamra'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh-cn'
+
+# Blogroll
+LINKS =  []
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+THEME = 'themes/notablog'
+
+CSS_FILE = "main.css"
+
+
+DEFAULT_PAGINATION = False
+
+DISPLAY_PAGES_ON_MENU = True
